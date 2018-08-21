7b5a0cf53df1e039619049f99d974064459b1a84
cx52018761 <cx52018761@hotmail.com>
2018-08-21 17:12:34 +0800

    修改路径

--- src/main/java/com/nicky/controller/UserController.java
+++ src/main/java/com/nicky/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller("testController")
-@RequestMapping("/jack")
+@RequestMapping("/user")
 public class UserController {
     
     @Autowired
